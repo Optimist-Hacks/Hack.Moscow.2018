2737e1299a5a471e1d144eabd6badf5bc65d37a7
otopba <otopba@inbox.ru>
2018-10-27 12:26:56 +0300

    add pas

--- src/main/java/com/hackdocs/controller/GoogleController.java
+++ src/main/java/com/hackdocs/controller/GoogleController.java
@@ -1,6 +1,9 @@
 package com.hackdocs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1/google")
 public class GoogleController {
 
+    private static Logger logger = LoggerFactory.getLogger(GoogleController.class);
+
     @PostMapping("")
-    public void process() {
+    public void process(@RequestBody String payload) {
+        logger.info("Receive google request payload = " + payload);
     }
 
 }
