f182046c2d86c763ddfbe8ce0df87a8bd935f145
Stepan Kladikov <inform.cosmo@yandex.ru>
2018-10-27 15:58:13 +0300

    small test

--- src/main/java/com/hackdocs/pfd/Temple.java
+++ src/main/java/com/hackdocs/pfd/Temple.java
@@ -3,7 +3,8 @@ package com.hackdocs.pfd;
 import com.itextpdf.text.DocumentException;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Temple {
 
@@ -11,7 +12,13 @@ public class Temple {
 
         PdfProcessor processor = new PdfProcessor();
 
-        //ArrayList<FieldModels> models = processor.process("src/main/resources/pdf/example3.pdf");
+        HashMap<Integer, FieldModels> models = processor.process("src/main/resources/pdf/example3.pdf");
+
+        for (Map.Entry<Integer, FieldModels> entry : models.entrySet()) {
+            processor.writeToLine(entry.getKey(), entry.getValue().toString());
+        }
+
+        processor.writeDocument();
 
     }
 
