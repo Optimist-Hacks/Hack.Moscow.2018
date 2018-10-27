f08c6ab35c60d77c1b6081c27278340eee70cb75
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-10-28 02:13:57 +0300

    test image

--- src/main/java/com/hackdocs/model/response/Payload.java
+++ src/main/java/com/hackdocs/model/response/Payload.java
@@ -11,6 +11,14 @@ public class Payload {
 
     public Telegram telegram;
 
+    public String messages = "[\n" +
+            "  {\n" +
+            "    \"imageUrl\": \"http://images.math.cnrs.fr/IMG/png/section8-image.png\",\n" +
+            "    \"platform\": \"google\",\n" +
+            "    \"type\": 3\n" +
+            "  }\n" +
+            "]";
+
 
 
     public Payload(Google google) {
