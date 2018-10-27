bb2e29dbf9290f635efb6b96966a93c37c5c219c
otopba <otopba@inbox.ru>
2018-10-27 16:53:37 +0300

    test class

---
+++ src/test/java/com/hackdocs/ValidatorTests.java
@@ -0,0 +1,15 @@
+package com.hackdocs;
+
+import com.hackdocs.validators.Validator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidatorTests {
+
+    @Test
+    public void validateNameTest() {
+        Assert.assertTrue(Validator.isValidFirstName("Ivan"));
+        Assert.assertFalse(Validator.isValidFirstName("123"));
+    }
+
+}
