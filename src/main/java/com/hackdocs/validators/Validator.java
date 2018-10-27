11a004b811f518b8f94a6adb768ba1a79bf1347f
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-10-27 16:26:23 +0300

    implement base name validation

--- src/main/java/com/hackdocs/validators/Validator.java
+++ src/main/java/com/hackdocs/validators/Validator.java
@@ -1,22 +1,38 @@
 package com.hackdocs.validators;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
 
-    public static boolean isValidFirstName(String firstName){
+    private static final Pattern firstNamePattern = Pattern.compile("[A-Z][a-zA-Z][^#&<>\\\"~;$^%{}?]{1,20}$");
+    private static final Pattern secondNamePattern = Pattern.compile("[A-Z][a-zA-Z][^#&<>\\\"~;$^%{}?]{1,20}$");
+
+    public static boolean isValidFirstName(String firstName) {
+        Matcher matcher = firstNamePattern.matcher(firstName);
+
+        if (matcher.find()) {
+            return matcher.start() == 0;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isValidSecondName(String secondName) {
+        Matcher matcher = secondNamePattern.matcher(secondName);
+
+        if (matcher.find()) {
+            return matcher.start() == 0;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isValidSex(String sex) {
         return true;
     }
 
-    public static boolean isValidSecondName(String secondName){
+    public static boolean isValidDate(String date) {
         return true;
     }
-
-    public static boolean isValidSex(String sex){
-        return true;
-    }
-
-    public static boolean isValidDate(String date){
-        return true;
-    }
-
-    public static boolean isValidFavoritePornField(String pornGenre) { return true; }
 }
