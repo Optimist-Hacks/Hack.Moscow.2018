9cb07dc628238247f3ae16d3a48caecdaad01413
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-10-28 02:09:09 +0300

    test image

--- src/main/java/com/hackdocs/model/response/payload/Google.java
+++ src/main/java/com/hackdocs/model/response/payload/Google.java
@@ -9,6 +9,16 @@ public class Google {
     public Boolean expectUserResponse;
     public RichResponse richResponse;
 
+    public String imageUrl = "http://images.math.cnrs.fr/IMG/png/section8-image.png";
+
+    public String messages = "[\n" +
+            "  {\n" +
+            "    \"imageUrl\": \"http://images.math.cnrs.fr/IMG/png/section8-image.png\",\n" +
+            "    \"platform\": \"google\",\n" +
+            "    \"type\": 3\n" +
+            "  }\n" +
+            "]";
+
     public Google(Boolean expectUserResponse, RichResponse richResponse) {
         this.expectUserResponse = expectUserResponse;
         this.richResponse = richResponse;
