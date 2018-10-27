7b82475b246cf4c999246feefd96a351defcf05c
otopba <otopba@inbox.ru>
2018-10-27 16:10:46 +0300

    docs process

--- src/main/java/com/hackdocs/service/VocationLogic.java
+++ src/main/java/com/hackdocs/service/VocationLogic.java
@@ -1,5 +1,6 @@
 package com.hackdocs.service;
 
+import com.hackdocs.validators.Validator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,14 +10,48 @@ public class VocationLogic extends AbstractFlow<VocationLogic.State> {
     public String process(String text, SessionState<VocationLogic.State> state) {
         switch (state.getLogicState()) {
             case INIT:
-                changeState(state, State.FIRST_NAME);
-                return "Hello! You want to create vocation document. What is your name?";
+                return handleInit(state);
             case FIRST_NAME:
-                changeState(state, State.LAST_NAME);
-                return "OK!";
+                return handleFirstName(text, state);
+            case LAST_NAME:
+                return handleLastName(text, state);
+            case DATE:
+                return handleDate(text, state);
         }
 
         return defaultResponse();
+    }
+
+    private String handleInit(SessionState<VocationLogic.State> state) {
+        changeState(state, State.FIRST_NAME);
+        return "Hello! You want to create vocation document. What is your name?";
+    }
+
+    private String handleFirstName(String text, SessionState<VocationLogic.State> state) {
+        if (!Validator.isValidFirstName(text)) {
+            return "This is not a name. Try again";
+        }
+
+        changeState(state, State.LAST_NAME);
+        return "Ok. Now, please enter last name";
+    }
+
+    private String handleLastName(String text, SessionState<VocationLogic.State> state) {
+        if (!Validator.isValidSecondName(text)) {
+            return "This is not a last name. Try again";
+        }
+
+        changeState(state, State.DATE);
+        return "Ok. Now, please enter a date";
+    }
+
+    private String handleDate(String text, SessionState<VocationLogic.State> state) {
+        if (!Validator.isValidDate(text)) {
+            return "This is not a date. Try again";
+        }
+
+        changeState(state, State.FIRST_NAME);
+        return "Ok. This is all. ";
     }
 
     @Override
@@ -25,13 +60,14 @@ public class VocationLogic extends AbstractFlow<VocationLogic.State> {
     }
 
     private String defaultResponse() {
-        return "OH MY GOD";
+        return "Something goes wrong. Please try again";
     }
 
     public enum State {
         INIT,
         FIRST_NAME,
         LAST_NAME,
+        DATE
     }
 
 }
