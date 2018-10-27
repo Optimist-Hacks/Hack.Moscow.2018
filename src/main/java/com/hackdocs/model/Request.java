153c13ccddd9aaa619cde85ebb5a464f011fb7d4
otopba <otopba@inbox.ru>
2018-10-27 19:00:33 +0300

    @JsonIgnoreProperties(ignoreUnknown = true)

--- src/main/java/com/hackdocs/model/Request.java
+++ src/main/java/com/hackdocs/model/Request.java
@@ -1,9 +1,11 @@
 package com.hackdocs.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.hackdocs.model.request.QueryResult;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Request {
 
     String responseId;
