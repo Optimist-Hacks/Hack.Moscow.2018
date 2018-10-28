41e7cf1694a919e63f31edbc185e09fd61deb759
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-10-28 05:11:37 +0300

    test image google

--- src/main/java/com/hackdocs/model/response/payload/google/expectedInputs/inputPrompt/richInitialPrompt/items/basicCard/button/OpenUrlAction.java
+++ src/main/java/com/hackdocs/model/response/payload/google/expectedInputs/inputPrompt/richInitialPrompt/items/basicCard/button/OpenUrlAction.java
@@ -1,8 +1,9 @@
 package com.hackdocs.model.response.payload.google.expectedInputs.inputPrompt.richInitialPrompt.items.basicCard.button;
 
+import lombok.Data;
+
+@Data
 public class OpenUrlAction {
 
     String url = "https://techdrive.pro/api/v1/pdf/1540689754158.pdf";
-
-
 }
