d2038b459b27d883d1da474a9b33bb11a3ed3340
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-10-28 04:57:34 +0300

    test image google

--- src/main/java/com/hackdocs/model/response/payload/google/expectedInputs/inputPrompt/richInitialPrompt/items/basicCard/Image.java
+++ src/main/java/com/hackdocs/model/response/payload/google/expectedInputs/inputPrompt/richInitialPrompt/items/basicCard/Image.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class Image {
 
-    String url = "http://images.math.cnrs.fr/IMG/png/section8-image.png";
+//    String url = "http://images.math.cnrs.fr/IMG/png/section8-image.png";
+    String url = "https://techdrive.pro/api/v1/pdf/1540689754158.pdf";
+
     String accessibilityText = "Image alternate text";
 }
