c15c506b28205aac295d7f24d0819ffa55d23450
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-10-28 04:14:19 +0300

    fix cancel

--- src/main/java/com/hackdocs/model/response/payload/Google.java
+++ src/main/java/com/hackdocs/model/response/payload/Google.java
@@ -1,5 +1,6 @@
 package com.hackdocs.model.response.payload;
 
+import com.hackdocs.model.response.payload.google.ExpectedInputs;
 import com.hackdocs.model.response.payload.google.RichResponse;
 import lombok.Data;
 
@@ -8,7 +9,7 @@ public class Google {
 
     public Boolean expectUserResponse;
     public RichResponse richResponse;
-    //public ExpectedInputs expectedInputs = new ExpectedInputs();
+    public ExpectedInputs expectedInputs = new ExpectedInputs();
 
     public Google(Boolean expectUserResponse, RichResponse richResponse) {
         this.expectUserResponse = expectUserResponse;
